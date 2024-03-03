0bf7c7584d8280c354100fb44b4d1455b6f7442a
haderie <haderie49@gmail.com>
2024-03-03 00:25:35 -0500

    page updates

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
-import logo from '../assets/logo.png'
+import logo2 from '../assets/logo2.png'
 import navIcon1 from '../assets/nav-icon1.svg'
 import navIcon3 from '../assets/dec.png'
 import email from '../assets/ema.jpg'
-import headerImg from "../assets/typing.avif";
-import PageComponents from '../components/pageComponent';
-import About from './About';
+import github from '../assets/github.png'
 
 const Navbar = () => {
   return (
     <section className='Navbar'>
 			<nav class="navbar background">
-				<ul class="nav-list">
 					<div class="logo">
                     <Link to="/Home">  {/* Use Link instead of a */}
-                        <img src={logo} alt="logo" />
+                        <img src={logo2} alt="logo" />
                     </Link>
 					</div>
-					<li><a href="/Home">Home</a></li>
-					<li><a href="/about">About Me </a></li>
-					<li><a href="/Projects" rout>Projects</a></li>
-					<li><a href='/student'>Student</a></li>
+          <ul class="nav-list">
+					<li><Link to="/Home">Home</Link></li>
+					<li><Link to="/about">About Me</Link></li>
+					<li><Link to="/Projects" >Projects</Link></li>
+					<li><Link to='/student'>Student</Link></li>
 				</ul>
         <div className="social-icon">
               <a href="https://www.linkedin.com/in/hareg-aderie-a4ab51288" target="_blank"><img src={navIcon1} alt="" /></a>
-              <a href=""><img src={email} alt="" /></a>
               <a href="#"><img src={navIcon3} alt="" /></a>
+              <a href="#"><img src={email} alt="" /></a>
+              <a href="#"><img src={github} alt="" /></a>
         </div>        
 			</nav>
 		
